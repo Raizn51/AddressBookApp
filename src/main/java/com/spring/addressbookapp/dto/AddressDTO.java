6560381c3fa1b16159bf571b0c76c890e657ae47
Raizn51 <Gouravraikwar989@gmail.com>
2025-03-07 15:11:33 +0530

    [Gourav Raikwar] Add . Add Validation to all Field so the REST call can be validated

--- src/main/java/com/spring/addressbookapp/dto/AddressDTO.java
+++ src/main/java/com/spring/addressbookapp/dto/AddressDTO.java
@@ -1,11 +1,26 @@
 package com.spring.addressbookapp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class AddressDTO {
+    @NotEmpty(message = "Name cannot be null")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name is Invalid")
     public String fullName;
+
+    @NotEmpty(message = "Address cannot be null")
     public String address;
+
+    @NotEmpty(message = "City cannot be null")
     public String city;
+
+    @NotEmpty(message = "State cannot be null")
     public String state;
+
+    @NotEmpty(message = "Zip Code cannot be null")
     public String zipCode;
+
+    @NotEmpty(message = "Phone Number cannot be null")
     public String phoneNumber;
 
     public AddressDTO(String fullName, String city, String state, String zipCode, String phoneNumber) {
